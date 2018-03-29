2e0756127e050eca64812da0b9fb3258a0754f24
Conway <s_conway69704@tss-hr420-dm12.fc.ca>
2018-03-29 11:07:47 -0400

    now do homework

--- js/main.js
+++ js/main.js
@@ -10,8 +10,40 @@ let sigils = document.querySelectorAll('.sigilContainer'),
     lightbox = document.querySelector('.lightbox'),
     closeLightBoxButton = document.querySelector('.close-lightbox'),
     vidPlayer = document.querySelector('video'),
-    vidControls = document.querySelector('.controls');
+    vidControls = document.querySelector('.controls'),
+    imageBanner = document.querySelector('#houseImages');
 //functions in the middle
+
+
+function showHouseVideo(){
+  let houseName = this.className.split(' ')[1].capIt();
+
+  document.querySelector('h1').textContent = `House ${houseName}`;
+    //debugger;
+  lightbox.classList.add('show-lightbox');
+  vidPlayer.src = `video/House-${houseName}.${vidPlayer.currentSrc.split('.')[1]}`;
+  vidPlayer.load();
+  //make it play
+  vidPlayer.play();
+
+  scrollBanners(this.dataset.offset);
+}
+
+function scrollBanners(offset) {
+  //move the banner images to the left
+   let moveIt = offset * 600 + "px";
+   imageBanner.style.right = moveIt;
+}
+
+
+function closeLightBox(){
+  lightbox.classList.remove('show-lightbox');
+  //stop the video and rewind it to 0
+  vidPlayer.pause();
+  vidPlayer.currentTime = 0;
+
+}
+
 function pausePlay(){
 
   let theButton = this.firstElementChild;
@@ -23,27 +55,6 @@ if (vidPlayer.paused == true){
     vidPlayer.pause();
     theButton.dataset.icon = "play-circle";
   }
-
-}
-
-function showHouseVideo(){
-  let houseName = this.className.split(' ')[1].capIt()
-
-  document.querySelector('h1').textContent = `House ${houseName}`;
-    //debugger;
-  lightbox.classList.add('show-lightbox');
-  vidPlayer.src = `video/House-${houseName}.${vidPlayer.currentSrc.split('.')[1]}`;
-  vidPlayer.load();
-  //make it play
-  vidPlayer.play();
-}
-
-
-function closeLightBox(){
-  lightbox.classList.remove('show-lightbox');
-  //stop the video and rewind it to 0
-  vidPlayer.pause();
-  vidPlayer.currentTime = 0;
 
 }
 
